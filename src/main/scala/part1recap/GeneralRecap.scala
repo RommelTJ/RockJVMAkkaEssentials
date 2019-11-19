edab835469935ee7febd7070a11a29e40c32b995
Rommel Rico <me@rommelrico.com>
2019-11-19 07:16:37 -0800

    :sparkles: Recap: Variables, Expressions, Code Blocks

--- src/main/scala/part1recap/GeneralRecap.scala
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -1,5 +1,18 @@
 package part1recap
 
 object GeneralRecap extends App {
+
+  val aCondition: Boolean = false
+  var aVariable = 42
+  aVariable += 1 // aVariable = 43
+
+  // expressions
+  val aConditionedVal = if (aCondition) 42 else 65 // 65
   
+  // code block
+  val aCodeBlock = {
+    if (aCondition) 74
+    56
+  } // 56
+
 }
