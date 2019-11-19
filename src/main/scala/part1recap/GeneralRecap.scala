858a26aed4234a5567cba51b49fef48d5c1051c8
Rommel Rico <me@rommelrico.com>
2019-11-19 07:17:42 -0800

    :sparkles: Recap: Unit Type

--- src/main/scala/part1recap/GeneralRecap.scala
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -8,11 +8,15 @@ object GeneralRecap extends App {
 
   // expressions
   val aConditionedVal = if (aCondition) 42 else 65 // 65
-  
+
   // code block
   val aCodeBlock = {
     if (aCondition) 74
     56
   } // 56
+
+  // types
+  // Unit
+  val theUnit = println("Hello, Scala")
 
 }
