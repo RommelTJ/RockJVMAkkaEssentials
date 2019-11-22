ff0d49cca9e6613ef72d20c2756fd277c0a0e7c8
Rommel Rico <me@rommelrico.com>
2019-11-22 07:21:37 -0800

    :sparkles: Recap: Multithreading on the JVM

--- src/main/scala/part1recap/MultithreadingRecap.scala
+++ src/main/scala/part1recap/MultithreadingRecap.scala
@@ -1,5 +1,14 @@
 package part1recap
 
 object MultithreadingRecap extends App {
-  
+
+  // Creating threads on the JVM
+
+  val aThread = new Thread(new Runnable {
+    override def run(): Unit = println("I'm running in parallel")
+  })
+  val aThread2 = new Thread(() => println("I'm running in parallel"))
+  aThread2.start()
+  aThread2.join()
+
 }
