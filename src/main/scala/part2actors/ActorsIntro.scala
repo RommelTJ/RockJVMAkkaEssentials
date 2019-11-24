3ba94ecb01f4ccfff40147448c39ec143b7cf695
Rommel Rico <me@rommelrico.com>
2019-11-24 07:16:34 -0800

    :sparkles: Actors: Rules of Actors

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -9,4 +9,12 @@ object ActorsIntro extends App {
   val actorSystem = ActorSystem("firstActorSystem")
   println(actorSystem.name)
 
+  // Part 2 - Create Actors
+
+  // Rules of Actors:
+  // Actors are uniquely identified
+  // Messages are asynchronous
+  // Each actor may respond differently
+  // Actors are encapsulated
+
 }
