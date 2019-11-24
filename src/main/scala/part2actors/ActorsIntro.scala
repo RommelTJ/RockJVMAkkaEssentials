f43655fd630f6039bbe4f9996567625333b32e1b
Rommel Rico <me@rommelrico.com>
2019-11-24 07:19:08 -0800

    :sparkles: Actors: Creating an actor

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -1,6 +1,6 @@
 package part2actors
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem}
 
 object ActorsIntro extends App {
 
@@ -16,5 +16,17 @@ object ActorsIntro extends App {
   // Messages are asynchronous
   // Each actor may respond differently
   // Actors are encapsulated
+
+  // word count actor
+  class WordCountActor extends Actor {
+    // internal data
+    var totalWords = 0
+
+    // behavior
+    override def receive: PartialFunction[Any, Unit] = {
+      case message: String => totalWords += message.split(" ").length
+      case msg => println(s"[word counter] I cannot understand ${msg.toString}")
+    }
+  }
 
 }
