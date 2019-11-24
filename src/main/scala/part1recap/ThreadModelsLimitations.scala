da45c5a99c966cc4f403219f8f1272e71f2a31b8
Rommel Rico <me@rommelrico.com>
2019-11-23 19:15:20 -0800

    :sparkles: Recap: Delegating something to a thread is a pain

--- src/main/scala/part1recap/ThreadModelsLimitations.scala
+++ src/main/scala/part1recap/ThreadModelsLimitations.scala
@@ -12,14 +12,14 @@ object ThreadModelsLimitations extends App {
     def getAmount(): String = s"$amount"
   }
 
-  val account = new BankAccount(2000)
-  for (_ <- 1 to 1000) {
-    new Thread(() => account.withdraw(1)).start()
-  }
-  for (_ <- 1 to 1000) {
-    new Thread(() => account.deposit(1)).start()
-  }
-  println(account.getAmount()) // result: 1,998.
+//  val account = new BankAccount(2000)
+//  for (_ <- 1 to 1000) {
+//    new Thread(() => account.withdraw(1)).start()
+//  }
+//  for (_ <- 1 to 1000) {
+//    new Thread(() => account.deposit(1)).start()
+//  }
+//  println(account.getAmount()) // result: 1,998.
   // OOP encapsulation is broken in a multithreading environment.
   // To fix, involves encapsulation. Locks to the rescue.
 
@@ -34,15 +34,50 @@ object ThreadModelsLimitations extends App {
     def getAmount(): String = s"$amount"
   }
 
-  val account2 = new BankAccount2(2000)
-  for (_ <- 1 to 1000) {
-    new Thread(() => account2.withdraw(1)).start()
-  }
-  for (_ <- 1 to 1000) {
-    new Thread(() => account2.deposit(1)).start()
-  }
-  println(account2.getAmount()) // result: 1,999.
+//  val account2 = new BankAccount2(2000)
+//  for (_ <- 1 to 1000) {
+//    new Thread(() => account2.withdraw(1)).start()
+//  }
+//  for (_ <- 1 to 1000) {
+//    new Thread(() => account2.deposit(1)).start()
+//  }
+//  println(account2.getAmount()) // result: 1,999.
 
   // But this also introduces deadlocks, livelocks and complex code.
+
+  // 2 - Delegating something to a thread is a pain.
+
+  // You have a running thread and you want to pass a Runnable to that thread. How?
+  var task: Runnable = null;
+  val runningThread: Thread = new Thread(() => {
+    while (true) {
+      while (task == null) {
+        runningThread.synchronized {
+          println("[background] waiting for a task...")
+          runningThread.wait()
+        }
+      }
+
+      task.synchronized {
+        println("[background] I have a task!")
+        task.run()
+        task = null
+      }
+    }
+  })
+
+  def delegateToBackgroundThread(r: Runnable): Unit = {
+    if (task == null) task = r
+
+    runningThread.synchronized {
+      runningThread.notify()
+    }
+  }
+
+  runningThread.start()
+  Thread.sleep(500)
+  delegateToBackgroundThread(() => println("42"))
+  Thread.sleep(1000)
+  delegateToBackgroundThread(() => println("This should run in the background"))
 
 }
