4c7711c102af7efd8069945b4751f878b1125ab6
Rommel Rico <me@rommelrico.com>
2019-11-24 07:13:52 -0800

    :sparkles: Actors: Creating our first actor system

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -1,5 +1,12 @@
 package part2actors
 
+import akka.actor.ActorSystem
+
 object ActorsIntro extends App {
-  
+
+  // Part 1 - Actor Systems
+
+  val actorSystem = ActorSystem("firstActorSystem")
+  println(actorSystem.name)
+
 }
