4d2bb35060da1f95c3f63ab03f65c5e5021d4080
Rommel Rico <me@rommelrico.com>
2019-11-25 07:05:19 -0800

    :sparkles: Actors: Messages can be of any type

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -7,6 +7,7 @@ object ActorCapabilities extends App {
   class SimpleActor extends Actor {
     override def receive: Receive = {
       case message: String => println(s"[simple actor] I have received $message")
+      case number: Int => println(s"[simple actor] I have received a number: $number")
     }
   }
 
@@ -14,5 +15,8 @@ object ActorCapabilities extends App {
 
   val simpleActor = system.actorOf(Props[SimpleActor], "simpleActor")
   simpleActor ! "hello, actor"
+
+  // 1 - messages can be of any type
+  simpleActor ! 42
 
 }
