961693fb6d330271a9a2a81c7c931abfe4d7624a
Rommel Rico <me@rommelrico.com>
2019-11-25 07:02:27 -0800

    :sparkles: Actors: Defining a simple actor

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,5 +1,13 @@
 package part2actors
 
+import akka.actor.Actor
+
 object ActorCapabilities extends App {
-  
+
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case message: String => println(s"[simple actor] I have received $message")
+    }
+  }
+
 }
