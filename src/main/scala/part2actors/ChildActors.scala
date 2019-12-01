3095b0ecce17b8a7f7026a0e8b2878a04413acb3
Rommel Rico <me@rommelrico.com>
2019-12-01 12:32:15 -0800

    :sparkles: Actors: Parent Actor

--- src/main/scala/part2actors/ChildActors.scala
+++ src/main/scala/part2actors/ChildActors.scala
@@ -1,5 +1,13 @@
 package part2actors
 
+import akka.actor.Actor
+
 object ChildActors extends App {
+
+  // Actors can create other Actors
+
+  class Parent extends Actor {
+    override def receive: Receive = ???
+  }
 
 }
