bd26bb8dede498eac533a5c2d1f2e5ed29c0456e
Rommel Rico <me@rommelrico.com>
2019-12-02 06:30:53 -0800

    :sparkles: Actors: WordCounterMaster

--- src/main/scala/part2actors/ChildActorsExercise.scala
+++ src/main/scala/part2actors/ChildActorsExercise.scala
@@ -1,5 +1,13 @@
 package part2actors
 
+import akka.actor.Actor
+
 object ChildActorsExercise extends App {
+
+  // Distributed Word Counting
+
+  class WordCounterMaster extends Actor {
+    override def receive: Receive = ???
+  }
 
 }
