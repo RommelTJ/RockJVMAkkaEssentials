aff04ce76a86731da64c4b3192c442d23e913083
Rommel Rico <me@rommelrico.com>
2019-12-02 06:35:42 -0800

    :pencil2: Actors: Flow of exercise

--- src/main/scala/part2actors/ChildActorsExercise.scala
+++ src/main/scala/part2actors/ChildActorsExercise.scala
@@ -19,4 +19,17 @@ object ChildActorsExercise extends App {
     override def receive: Receive = ???
   }
 
+  /*
+  Flow of exercise:
+  Create a WordCounterMaster
+  Send Initialize(10) to WordCounterMaster -> Create 10 workers
+  Send "Akka is awesome" to wordCounterMaster
+  -> WCM will send a WordCountTask("...") to one of its children
+   -> Child replies with a WordCountReply(3) to the master
+  -> Master replies with 3 to the sender
+
+  requester -> WCM -> WCW
+          r <- WCM <-
+   */
+
 }
