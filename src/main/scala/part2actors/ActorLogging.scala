7c10dd317837eb535a18a7ce8b0e8f60363d98d6
Rommel Rico <me@rommelrico.com>
2019-12-04 06:40:01 -0800

    :sparkles: Actors: Adding type annotation

--- src/main/scala/part2actors/ActorLogging.scala
+++ src/main/scala/part2actors/ActorLogging.scala
@@ -1,12 +1,12 @@
 package part2actors
 
 import akka.actor.{Actor, ActorSystem, Props}
-import akka.event.Logging
+import akka.event.{Logging, LoggingAdapter}
 
 object ActorLogging extends App {
 
   class SimpleActorWithExplicitLogger extends Actor {
-    val logger = Logging(context.system, this) // 1 - DEBUG, 2 - INFO, 3 - WARNING, 4 - ERROR
+    val logger: LoggingAdapter = Logging(context.system, this) // 1 - DEBUG, 2 - INFO, 3 - WARNING, 4 - ERROR
 
     override def receive: Receive = {
       case message => logger.info(message.toString) // LOG it
