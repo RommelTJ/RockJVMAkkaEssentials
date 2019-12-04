3f81279b70ad9a71c275fbdd81d68bb161e41e12
Rommel Rico <me@rommelrico.com>
2019-12-04 06:41:31 -0800

    :sparkles: Actors: ActorWithLogging

--- src/main/scala/part2actors/ActorLogging.scala
+++ src/main/scala/part2actors/ActorLogging.scala
@@ -1,10 +1,11 @@
 package part2actors
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.event.{Logging, LoggingAdapter}
 
 object ActorLogging extends App {
 
+  // Method #1: Explicit Logging
   class SimpleActorWithExplicitLogger extends Actor {
     val logger: LoggingAdapter = Logging(context.system, this) // 1 - DEBUG, 2 - INFO, 3 - WARNING, 4 - ERROR
 
@@ -17,5 +18,10 @@ object ActorLogging extends App {
   val system = ActorSystem("LoggingDemo")
   val simpleActorWithExplicitLogger = system.actorOf(Props[SimpleActorWithExplicitLogger])
   simpleActorWithExplicitLogger ! "Logging a simple message"
+
+  // Method #2: Actor Logging
+  class ActorWithLogging extends Actor with ActorLogging {
+    override def receive: Receive = ???
+  }
 
 }
