a21f85c674f43cf376239cd06de801715670d7f9
Rommel Rico <me@rommelrico.com>
2019-12-04 06:37:49 -0800

    :sparkles: Actors: Using explicit logging

--- src/main/scala/part2actors/ActorLogging.scala
+++ src/main/scala/part2actors/ActorLogging.scala
@@ -1,11 +1,16 @@
 package part2actors
 
 import akka.actor.Actor
+import akka.event.Logging
 
 object ActorLogging extends App {
 
   class SimpleActorWithExplicitLogger extends Actor {
-    override def receive: Receive = ???
+    val logger = Logging(context.system, this) // 1 - DEBUG, 2 - INFO, 3 - WARNING, 4 - ERROR
+
+    override def receive: Receive = {
+      case message => logger.info(message.toString) // LOG it
+    }
   }
 
 }
