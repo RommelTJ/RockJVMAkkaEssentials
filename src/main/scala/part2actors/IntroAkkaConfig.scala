1ba955ca87141b1d718ac0c53fb352d3850554d6
Rommel Rico <me@rommelrico.com>
2019-12-05 06:39:25 -0800

    :sparkles: Actors: Inline Configurations

--- src/main/scala/part2actors/IntroAkkaConfig.scala
+++ src/main/scala/part2actors/IntroAkkaConfig.scala
@@ -1,5 +1,18 @@
 package part2actors
 
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+
 object IntroAkkaConfig extends App {
+
+  // Method 1 - Inline Configuration
+  val configString =
+    """
+      | akka {
+      |   loglevel = "DEBUG"
+      | }
+      |""".stripMargin
+  val config = ConfigFactory.parseString(configString)
+  val system = ActorSystem("ConfigurationDemo", ConfigFactory.load(config))
 
 }
