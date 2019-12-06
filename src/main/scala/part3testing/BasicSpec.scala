90be98775d247c39b84f7ccb6f2ff1e91a1ef31b
Rommel Rico <me@rommelrico.com>
2019-12-06 07:20:16 -0800

    :sparkles: Testing: Basic Setup

--- src/main/scala/part3testing/BasicSpec.scala
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -1,5 +1,17 @@
 package part3testing
 
-class BasicSpec {
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{WordSpecLike, BeforeAndAfterAll}
+
+class BasicSpec extends TestKit(ActorSystem("BasicSpec"))
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
+  // Setup
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
 
 }
