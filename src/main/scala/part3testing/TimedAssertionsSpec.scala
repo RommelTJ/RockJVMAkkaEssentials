2cb8535becb6743353602b0cd25690535a7f69ed
Rommel Rico <me@rommelrico.com>
2019-12-09 07:09:31 -0800

    :tada: Testing: TimedAssertionsSpec

---
+++ src/main/scala/part3testing/TimedAssertionsSpec.scala
@@ -0,0 +1,23 @@
+package part3testing
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class TimedAssertionsSpec extends TestKit(ActorSystem("TimedAssertionsSpec"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import TimedAssertionsSpec._
+
+}
+
+object TimedAssertionsSpec {
+  // testing scenario
+}
