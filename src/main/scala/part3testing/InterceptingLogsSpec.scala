bd3cae18653b65e48d5d79eca1d868c106a39170
Rommel Rico <me@rommelrico.com>
2019-12-12 06:27:49 -0800

    :tada: Testing: InterceptingLogsSpec

---
+++ src/main/scala/part3testing/InterceptingLogsSpec.scala
@@ -0,0 +1,21 @@
+package part3testing
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class InterceptingLogsSpec extends TestKit(ActorSystem("InterceptingLogsSpec"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+}
+
+object InterceptingLogsSpec {
+
+}
