a4e0e5144e0edb4b89e09fc3a201124a6d247ec4
Rommel Rico <me@rommelrico.com>
2019-12-13 07:10:15 -0800

    :tada: Testing: SynchronousTestingSpec

---
+++ src/main/scala/part3testing/SynchronousTestingSpec.scala
@@ -0,0 +1,12 @@
+package part3testing
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class SynchronousTestingSpec extends AnyWordSpecLike with BeforeAndAfterAll {
+
+}
+
+object SynchronousTestingSpec {
+
+}
