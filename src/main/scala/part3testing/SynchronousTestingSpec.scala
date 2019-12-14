f3eded28a74d753526909ffd652925023eed5d80
Rommel Rico <me@rommelrico.com>
2019-12-14 12:02:11 -0800

    :sparkles: Testing: Testing scenario

--- src/main/scala/part3testing/SynchronousTestingSpec.scala
+++ src/main/scala/part3testing/SynchronousTestingSpec.scala
@@ -1,5 +1,6 @@
 package part3testing
 
+import akka.actor.Actor
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
 
@@ -8,5 +9,17 @@ class SynchronousTestingSpec extends AnyWordSpecLike with BeforeAndAfterAll {
 }
 
 object SynchronousTestingSpec {
+
+  case object Inc
+  case object Read
+
+  class Counter extends Actor {
+    var count = 0
+
+    override def receive: Receive = {
+      case Inc => count += 1
+      case Read => sender() ! count
+    }
+  }
 
 }
