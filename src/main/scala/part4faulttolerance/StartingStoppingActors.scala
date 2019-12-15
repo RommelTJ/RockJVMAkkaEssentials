d349edc23aef4cb9081a7290edd01a698f2944dc
Rommel Rico <me@rommelrico.com>
2019-12-15 11:36:04 -0800

    :sparkles: FaulTolerance: Testing stopping yourself and your children

--- src/main/scala/part4faulttolerance/StartingStoppingActors.scala
+++ src/main/scala/part4faulttolerance/StartingStoppingActors.scala
@@ -21,6 +21,8 @@ object StartingStoppingActors extends App {
       case Stop =>
         log.info(s"Stopping myself")
         context.stop(self)
+      case message =>
+        log.info(message.toString)
     }
   }
   object Parent {
@@ -43,6 +45,13 @@ object StartingStoppingActors extends App {
 
   parent ! StopChild("child1")
   // Note: Child stops asynchronously, so not immediately.
-  for (_ <- 1 to 50) child ! "are you still there?" // Will receive a few of those before DeadLetters.
+  // for (_ <- 1 to 50) child ! "are you still there?" // Will receive a few of those before DeadLetters.
+
+  parent ! StartChild("child2")
+  val child2 = system.actorSelection("/user/parent/child2")
+  child2 ! "Hi, Second child!"
+  parent ! Stop
+  for (i <- 1 to 10) parent ! s"[$i]Parent, are you still there?" // Are never received because Stop is sent first.
+  for (i <- 1 to 100) child2 ! s"[$i]Child, are you still there?" // Some are received.
 
 }
