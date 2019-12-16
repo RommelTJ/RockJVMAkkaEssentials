38ff53007cbd5782df56b95ea9cde7247bf27d8d
Rommel Rico <me@rommelrico.com>
2019-12-16 07:01:12 -0800

    :sparkles: FaultTolerance: Overriding lifecycle hooks

--- src/main/scala/part4faulttolerance/ActorLifecycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifecycle.scala
@@ -6,6 +6,10 @@ object ActorLifecycle extends App {
 
   object StartChild
   class LifecycleActor extends Actor with ActorLogging {
+    override def preStart(): Unit = log.info(s"I am starting...")
+
+    override def postStop(): Unit = log.info(s"I have stopped...")
+    
     override def receive: Receive = {
       case StartChild => context.actorOf(Props[LifecycleActor], "child")
     }
