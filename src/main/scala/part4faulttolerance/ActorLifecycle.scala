3147b0073ec36b3b7cfdaee9653adc70fd76c116
Rommel Rico <me@rommelrico.com>
2019-12-16 07:00:04 -0800

    :sparkles: FaultTolerance: LifecycleActor

--- src/main/scala/part4faulttolerance/ActorLifecycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifecycle.scala
@@ -1,7 +1,14 @@
 package part4faulttolerance
 
+import akka.actor.{Actor, ActorLogging, Props}
+
 object ActorLifecycle extends App {
 
-  
+  object StartChild
+  class LifecycleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case StartChild => context.actorOf(Props[LifecycleActor], "child")
+    }
+  }
 
 }
