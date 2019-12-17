343bca5cb4251ba06426ca83cf58ddd398393932
Rommel Rico <me@rommelrico.com>
2019-12-17 07:06:09 -0800

    :sparkles: FaultTolerance: FussyWordCounter Report

--- src/main/scala/part4faulttolerance/SupervisionSpec.scala
+++ src/main/scala/part4faulttolerance/SupervisionSpec.scala
@@ -20,15 +20,19 @@ class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
 
 object SupervisionSpec {
 
+  case object Report
+
   class FussyWordCounter extends Actor {
     var words = 0
 
     override def receive: Receive = {
+      case Report => sender() ! words
       case "" => throw new NullPointerException("Sentence is empty")
       case sentence: String =>
         if (sentence.length > 20) throw new RuntimeException("Sentence is too big")
         else if (!Character.isUpperCase(sentence(0))) throw new IllegalArgumentException("Sentence must start with uppercase")
         else words += sentence.split(" ").length
+      case _ => throw new Exception("Can only receive strings")
     }
   }
 
