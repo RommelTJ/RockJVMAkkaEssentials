5faf4f5abb39b291815f1b2dc6a169408146d039
Rommel Rico <me@rommelrico.com>
2019-12-17 07:05:06 -0800

    :sparkles: FaultTolerance: FussyWordCounter actor

--- src/main/scala/part4faulttolerance/SupervisionSpec.scala
+++ src/main/scala/part4faulttolerance/SupervisionSpec.scala
@@ -1,6 +1,6 @@
 package part4faulttolerance
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
@@ -19,5 +19,17 @@ class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
 }
 
 object SupervisionSpec {
+
+  class FussyWordCounter extends Actor {
+    var words = 0
+
+    override def receive: Receive = {
+      case "" => throw new NullPointerException("Sentence is empty")
+      case sentence: String =>
+        if (sentence.length > 20) throw new RuntimeException("Sentence is too big")
+        else if (!Character.isUpperCase(sentence(0))) throw new IllegalArgumentException("Sentence must start with uppercase")
+        else words += sentence.split(" ").length
+    }
+  }
 
 }
