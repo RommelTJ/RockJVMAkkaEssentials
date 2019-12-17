9df43f0b99dd591a7e2084ab9845fb2508235689
Rommel Rico <me@rommelrico.com>
2019-12-17 07:02:14 -0800

    :sparkles: FaultTolerance: Setting up the SupervisionSpec as a TestKit

--- src/main/scala/part4faulttolerance/SupervisionSpec.scala
+++ src/main/scala/part4faulttolerance/SupervisionSpec.scala
@@ -1,7 +1,21 @@
 package part4faulttolerance
 
-class SupervisionSpec {
-  
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import SupervisionSpec._
+
 }
 
 object SupervisionSpec {
