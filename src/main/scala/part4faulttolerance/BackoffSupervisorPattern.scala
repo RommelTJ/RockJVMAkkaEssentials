a037e947fdb67cc2e56cbbf319f0a991923f2a0f
Rommel Rico <me@rommelrico.com>
2019-12-18 06:58:36 -0800

    :sparkles: FaultTolerance: Implementing Receive in FileBasedPersistentActor

--- src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
+++ src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
@@ -1,12 +1,23 @@
 package part4faulttolerance
 
+import java.io.File
+
 import akka.actor.{Actor, ActorLogging}
+
+import scala.io.Source
 
 object BackoffSupervisorPattern extends App {
 
+  case object ReadFile
   class FileBasedPersistentActor extends Actor with ActorLogging {
-    case object ReadFile
-    override def receive: Receive = ???
+    var dataSource: Source = null
+
+    override def receive: Receive = {
+      case ReadFile =>
+        if (dataSource == null)
+          dataSource = Source.fromFile(new File("src/main/resources/testfiles/important.txt"))
+        log.info(s"I've just read some important data: ${dataSource.getLines().toList}")
+    }
   }
 
 }
