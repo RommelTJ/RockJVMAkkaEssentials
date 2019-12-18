baa4aff3a66e69a94114f3e5f6608a0d08a4495b
Rommel Rico <me@rommelrico.com>
2019-12-18 06:55:33 -0800

    :sparkles: FaultTolerance: FileBasedPersistentActor

--- src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
+++ src/main/scala/part4faulttolerance/BackoffSupervisorPattern.scala
@@ -1,5 +1,12 @@
 package part4faulttolerance
 
+import akka.actor.{Actor, ActorLogging}
+
 object BackoffSupervisorPattern extends App {
+
+  class FileBasedPersistentActor extends Actor with ActorLogging {
+    case object ReadFile
+    override def receive: Receive = ???
+  }
 
 }
