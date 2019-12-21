1d62562e57133e9e162ada1877ee1885121de638
Rommel Rico <me@rommelrico.com>
2019-12-21 14:20:38 -0800

    :sparkles: Infrastructure: SimpleActor

--- src/main/scala/part5infrastructure/TimersSchedulers.scala
+++ src/main/scala/part5infrastructure/TimersSchedulers.scala
@@ -1,5 +1,13 @@
 package part5infrastructure
 
+import akka.actor.{Actor, ActorLogging}
+
 object TimersSchedulers extends App {
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
 
 }
