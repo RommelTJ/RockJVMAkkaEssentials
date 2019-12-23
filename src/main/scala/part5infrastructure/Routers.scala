33d2ad1f857255f12dd6f6e0b22b15cb92e009ce
Rommel Rico <me@rommelrico.com>
2019-12-22 19:53:33 -0800

    :sparkles: Infrastructure: Child Actor

--- src/main/scala/part5infrastructure/Routers.scala
+++ src/main/scala/part5infrastructure/Routers.scala
@@ -1,11 +1,17 @@
 package part5infrastructure
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorLogging}
 
 object Routers extends App {
 
   class Master extends Actor {
     override def receive: Receive = ???
+  }
+
+  class Slave extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
   }
 
 }
