f309b6e727314e35073146d194684857f3eddbd7
Rommel Rico <me@rommelrico.com>
2019-12-22 19:56:53 -0800

    :sparkles: Infrastructure: Creating a Manual Router

--- src/main/scala/part5infrastructure/Routers.scala
+++ src/main/scala/part5infrastructure/Routers.scala
@@ -1,10 +1,23 @@
 package part5infrastructure
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
 
 object Routers extends App {
 
+  /*
+  Method #1 - Manual Router
+   */
   class Master extends Actor {
+    // 5 actor routees based off Slave Actors
+    private val slaves = for (_ <- 1 to 5) yield {
+      val slave = context.actorOf(Props[Slave])
+      context.watch(slave)
+      ActorRefRoutee(slave)
+    }
+
+    private val router = Router(RoundRobinRoutingLogic(), slaves)
+
     override def receive: Receive = ???
   }
 
