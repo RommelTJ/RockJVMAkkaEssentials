21954b737331f716242cd8d5328427b5c6085fce
Rommel Rico <me@rommelrico.com>
2019-12-23 06:35:36 -0800

    :sparkles: Infrastructure: Counter Actor

--- src/main/scala/part5infrastructure/Dispatchers.scala
+++ src/main/scala/part5infrastructure/Dispatchers.scala
@@ -1,5 +1,17 @@
 package part5infrastructure
 
+import akka.actor.{Actor, ActorLogging}
+
 object Dispatchers extends App {
+
+  class Counter extends Actor with ActorLogging {
+    var count = 0
+
+    override def receive: Receive = {
+      case message =>
+        count += 1
+        log.info(s"[$count] ${message.toString}")
+    }
+  }
 
 }
