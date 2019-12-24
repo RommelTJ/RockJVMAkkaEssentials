d5884abd55038aff81eb18794168b87357844223
Rommel Rico <me@rommelrico.com>
2019-12-24 10:59:24 -0800

    :sparkles: Infrastructure: SupportTicketPriorityMailbox

--- src/main/scala/part5infrastructure/Mailboxes.scala
+++ src/main/scala/part5infrastructure/Mailboxes.scala
@@ -1,6 +1,8 @@
 package part5infrastructure
 
 import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import com.typesafe.config.Config
 
 object Mailboxes extends App {
 
@@ -11,5 +13,23 @@ object Mailboxes extends App {
       case message => log.info(message.toString)
     }
   }
+
+  /**
+   * Interesting Case #1 - Custom priority mailbox
+   * P0 -> most important
+   * P1
+   * P2
+   * P3 -> least important
+   */
+  class SupportTicketPriorityMailbox(settings: ActorSystem.Settings, config: Config)
+    extends UnboundedPriorityMailbox(
+      PriorityGenerator {
+        case message: String if message.startsWith("[P0]") => 0
+        case message: String if message.startsWith("[P1]") => 1
+        case message: String if message.startsWith("[P2]") => 2
+        case message: String if message.startsWith("[P3]") => 3
+        case _ => 4
+      }
+    )
 
 }
