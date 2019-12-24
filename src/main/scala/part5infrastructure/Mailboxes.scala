5038f31ab37f4d6a02c5f448ef37d4f749ba6842
Rommel Rico <me@rommelrico.com>
2019-12-24 10:56:12 -0800

    :sparkles: Infrastructure: SimpleActor

--- src/main/scala/part5infrastructure/Mailboxes.scala
+++ src/main/scala/part5infrastructure/Mailboxes.scala
@@ -1,9 +1,15 @@
 package part5infrastructure
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 
 object Mailboxes extends App {
 
   val system = ActorSystem("MailboxDemo")
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
 
 }
