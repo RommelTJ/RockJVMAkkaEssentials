2b6d884601242c28e23fbcdf9594dcdb36854ce9
Rommel Rico <me@rommelrico.com>
2019-12-26 15:52:24 -0800

    :sparkles: Patterns: AuthManager Actor

--- src/main/scala/part6patterns/AskSpec.scala
+++ src/main/scala/part6patterns/AskSpec.scala
@@ -1,9 +1,16 @@
 package part6patterns
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.Success
 
 class AskSpec extends TestKit(ActorSystem("AskSpec"))
   with ImplicitSender
@@ -34,7 +41,33 @@ object AskSpec {
         log.info(s"Writing value: $value for key: $key")
         context.become(online(kv + (key -> value)))
     }
+  }
 
+  // User authenticator actor
+  case class RegisterUser(username: String, password: String)
+  case class Authenticate(username: String, password: String)
+  case class AuthFailure(message: String)
+  case object AuthSuccess
+
+  class AuthManager extends Actor with ActorLogging {
+    implicit val timeout: Timeout = Timeout(1 second)
+    implicit val executionContext: ExecutionContext = context.dispatcher
+
+    private val authDb = context.actorOf(Props[KVActor])
+
+    override def receive: Receive = {
+      case RegisterUser(username, password) =>
+        authDb ! Write(username, password)
+      case Authenticate(username, password) =>
+        val future = authDb ? Read(username)
+        future.onComplete {
+          case Success(None) =>
+            sender() ! AuthFailure("Username not found")
+          case Success(Some(dbPassword)) =>
+            if (dbPassword == password) sender() ! AuthSuccess
+            else sender() ! AuthFailure("Password incorrect.")
+        }
+    }
   }
 
 }
