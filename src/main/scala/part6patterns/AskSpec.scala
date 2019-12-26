be4560036d50b52d4b850f16692dc44ebe7d266e
Rommel Rico <me@rommelrico.com>
2019-12-26 15:21:42 -0800

    :sparkles: Patterns: AskSpec setup

--- src/main/scala/part6patterns/AskSpec.scala
+++ src/main/scala/part6patterns/AskSpec.scala
@@ -1,6 +1,20 @@
 package part6patterns
 
-class AskSpec {
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class AskSpec extends TestKit(ActorSystem("AskSpec"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import AskSpec._
 
 }
 
