d248cde9e14a6146dba6516e6d75f0ca460f49dd
Rommel Rico <me@rommelrico.com>
2019-12-28 22:37:36 -0800

    :sparkles: Patterns: Scaffolding for the FSMSpec

--- src/main/scala/part6patterns/FSMSpec.scala
+++ src/main/scala/part6patterns/FSMSpec.scala
@@ -1,8 +1,21 @@
 package part6patterns
 
-class FSMSpec {
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class FSMSpec extends TestKit(ActorSystem("FSMSpec"))
+  with ImplicitSender with AnyWordSpecLike with BeforeAndAfterAll {
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import FSMSpec._
 
 }
+
 object FSMSpec {
 
 }
